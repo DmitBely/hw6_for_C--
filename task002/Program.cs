8e6ec41ddb4b6a557c03842b7c124d7e97b1cd3d
dmitriy <dmitbely@protonmail.ch>
2022-09-08 20:09:25 +0300

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void PrintArray(int[,] col)
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int j = 0; j < col.GetLength(1); j++)
+        {
+            Console.Write($"{col[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+
+int m = 5;
+int n = 6;
+
+int[,] A = new int[m,n];
+
+for (int i = 0; i < A.GetLength(0); i++)
+{
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        A[i,j] = m + n;
+    }
+}
+
+PrintArray(A);
