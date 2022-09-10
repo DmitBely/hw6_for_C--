41f8aa5b5e5240003ef668abc3865c7b785cf794
dmitriy <dmitbely@protonmail.ch>
2022-09-10 22:39:44 +0300

    tassk006

---
+++ task006/Program.cs
@@ -0,0 +1,55 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);//[1; 10)
+        }
+    }
+}
+
+void SortArray(int[,] array)//сортировка строк массива по убыванию
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int maxPosition = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, maxPosition])
+                {
+                    maxPosition = k;
+                }
+            }
+            int temporary = array[i, j];
+            array[i, j] = array[i, maxPosition];
+            array[i, maxPosition] = temporary;
+        }
+
+    }
+}
+
+int[,] matrix = new int[5,5];
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+SortArray(matrix);
+PrintArray(matrix);
